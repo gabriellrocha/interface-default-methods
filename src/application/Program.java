36b9e573f21634b7bd961d377b2c271bafda18db
Gabriel Rocha <gabriell_rsilva@outlook.com>
2023-02-20 10:01:08 -0300

    completed

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,28 +10,31 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		double amountDouble;
 		
 		System.out.print("Amount: ");
 		String amount = input.next();
-		if (amount.contains(",")) {
-			amountDouble = Double.parseDouble(amount.replace(",", "."));
-		} else {
-			amountDouble = Double.parseDouble(amount);
-		}
+		
 		System.out.print("Months: ");
 		int months = input.nextInt();
 		
 		InterestService in = new BrazilInterestService();
+		
 		try {
+			double amountDouble = replaceComma(amount);
 			double payment = in.payment(amountDouble, months);			
 			System.out.print("Payment after " + months + " months: ");
 			System.out.println(String.format("%.2f", payment));
 		} catch(IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 		}
-		
 		input.close();
 	}
-
+	
+	public static double replaceComma(String amount) { 
+		if (amount.contains(",")) {
+			return Double.parseDouble(amount.replace(",", "."));
+		} 
+		return Double.parseDouble(amount);
+		
+	}
 }
